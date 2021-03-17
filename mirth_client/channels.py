1678659951a75b911fb6c503fd58f4dc7fd86b4c
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-03-17 17:07:29 +0000

    Handle cases where lists are expected but single item is returned

--- mirth_client/channels.py
+++ mirth_client/channels.py
@@ -1,23 +1,36 @@
-from typing import Optional, Dict
+from typing import TYPE_CHECKING, Dict, List, Optional, Union
 from uuid import UUID
 from xml.etree.ElementTree import Element, SubElement, tostring
 
 from .models import ChannelMessage, ChannelStatistics
+
+if TYPE_CHECKING:
+    from .mirth import MirthAPI
 
 
 def parse_channel_message(xml_dict: Dict):
     """
     Constructs a ChannelMessage object from a dictionary representation of Mirth Channel message XML
     """
+    connector_messages: Union[List, Dict] = xml_dict.get("connectorMessages", {}).get(
+        "entry", []
+    )
+
     message_dict = {
         "messageId": xml_dict.get("messageId"),
         "serverId": xml_dict.get("serverId"),
         "processed": xml_dict.get("processed"),
-        "connectorMessages": [
-            entry.get("connectorMessage")
-            for entry in xml_dict.get("connectorMessages", {}).get("entry", [])
-        ],
     }
+
+    if connector_messages and isinstance(connector_messages, list):
+        message_dict["connectorMessages"] = [
+            entry.get("connectorMessage") for entry in connector_messages
+        ]
+    elif connector_messages and isinstance(connector_messages, dict):
+        message_dict["connectorMessages"] = [connector_messages.get("connectorMessage")]
+    else:
+        message_dict["connectorMessages"] = []
+
     return ChannelMessage(**message_dict)
 
 
@@ -52,14 +65,29 @@ class Channel:
         return ChannelStatistics(**self.mirth.parse(r).get("channelStatistics"))
 
     def get_messages(
-        self, limit: int = 20, offset: int = 0, include_content: bool = True
+        self,
+        limit: int = 20,
+        offset: int = 0,
+        include_content: bool = True,
+        status: Optional[str] = None,
     ):
         params = {"limit": limit, "offset": offset, "includeContent": include_content}
+
+        if status:
+            params["status"] = status.upper()
+
         r = self.mirth.get(f"/channels/{self.id}/messages", params=params)
-        return [
-            parse_channel_message(message_dict)
-            for message_dict in self.mirth.parse(r).get("list", {}).get("message", [])
-        ]
+        messages: Union[List, Dict] = (
+            self.mirth.parse(r).get("list", {}).get("message", [])
+        )
+
+        # XML parser returns a list ONLY if more than 1 message is present
+        # otherwise it just returns the message itself. We have to account for this.
+        if messages and isinstance(messages, list):
+            return [parse_channel_message(message_dict) for message_dict in messages]
+        if messages and isinstance(messages, dict):
+            return [parse_channel_message(messages)]
+        return []
 
     def get_message(self, id_: str, include_content: bool = True):
         params = {"includeContent": include_content}
