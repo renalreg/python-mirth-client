a79e8dd53dd061229115b5549d104b81429b2871
OliveraReeves <oliver.reeves@ukkidney.org>
2026-07-14 14:03:10 +0100

    fix

--- mirth_client/models.py
+++ mirth_client/models.py
@@ -278,8 +278,9 @@ class MirthDatetime(datetime):
     def __get_pydantic_core_schema__(cls, source_type, handler):
         from pydantic_core import core_schema
 
-        return core_schema.no_info_plain_validator_function(
-            cls.validate,
+        return core_schema.json_or_python_schema(
+            json_schema=core_schema.str_schema(),
+            python_schema=core_schema.no_info_plain_validator_function(cls.validate),
             serialization=core_schema.plain_serializer_function_ser_schema(
                 lambda v: v.isoformat(),
                 when_used="json",
